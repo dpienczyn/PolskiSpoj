a1cdcce379c38735402c8d603745823e612c9434
dpienczyn <donia19881@wp.pl>
2019-08-27 20:40:11 +0200

    Add files via upload

---
+++ SystemyPozycyjne.java
@@ -0,0 +1,18 @@
+package rozmowa;
+
+import java.util.Scanner;
+
+public class SystemyPozycyjne {
+
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        int ile = s.nextInt();
+        for (int i = 0; i < ile; i++) {
+            int z = s.nextInt();
+            String dec = Integer.toHexString(z);
+            String j = Integer.toString(z, 11);
+            System.out.println(dec.toUpperCase() + " " + j.toUpperCase());
+        }
+    }
+
+}
