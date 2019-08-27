6e9d693dc844ee6c0df345da9a84f9286a4d31ac
dpienczyn <donia19881@wp.pl>
2019-08-27 20:41:39 +0200

    Update SystemyPozycyjne.java

--- SystemyPozycyjne.java
+++ SystemyPozycyjne.java
@@ -10,7 +10,7 @@ public class SystemyPozycyjne {
         for (int i = 0; i < ile; i++) {
             int z = s.nextInt();
             String dec = Integer.toHexString(z);
-            String j = Integer.toString(z, 11);
+            String j = Integer.toString(z, 11);  //system jednastkowy
             System.out.println(dec.toUpperCase() + " " + j.toUpperCase());
         }
     }
